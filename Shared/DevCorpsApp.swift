6899ebb06a143c0e4b4bb7ddaf347883c08d31cc
ttupper92618 <thomastupper92618@gmail.com>
2020-12-14 09:38:47 -0600

    initial

---
+++ Shared/DevCorpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevCorpsApp.swift
+//  Shared
+//
+//  Created by Thomas Tupper on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct DevCorpsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
